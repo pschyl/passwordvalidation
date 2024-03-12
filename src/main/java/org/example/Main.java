874a03e31309d19b97d70b82c6117b48b7317b59
Paul Schüller <136228264+pschyl@users.noreply.github.com>
2024-03-12 14:26:23 +0100

    create method placeholder and test file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static boolean checkLength(String password) {
+
+        return false;
+    }
+
+    public static boolean hasDigit(String password) {
+
+        return false;
+    }
+
+    public static boolean checkCases(String password) {
+
+        return false;
+    }
+
+    public static boolean checkCommon(String password) {
+
+        return false;
+    }
 }
