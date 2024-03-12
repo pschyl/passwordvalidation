7762fc90141ec0e587600c9e80153101ee5ca9a2
Paul Schüller <136228264+pschyl@users.noreply.github.com>
2024-03-12 15:18:12 +0100

    implement tests for hasDigit

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -27,4 +27,24 @@ class MainTest {
         Assertions.assertFalse(actual);
     }
 
+    @Test
+    void hasDigit_shouldReturnTrue_whenCalledWith_abcdef3() {
+        //GIVEN
+        String input = "abcdef3";
+        //WHEN
+        boolean actual = Main.hasDigit(input);
+        //THEN
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void hasDigit_shouldReturnFalse_whenCalledWith_abcdef() {
+        //GIVEN
+        String input = "abcdef";
+        //WHEN
+        boolean actual = Main.hasDigit(input);
+        //THEN
+        Assertions.assertFalse(actual);
+    }
+
 }
