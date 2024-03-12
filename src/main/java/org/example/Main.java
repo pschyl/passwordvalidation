22ada975401005c70681378a4e075b26aa67bd83
Paul Schüller <136228264+pschyl@users.noreply.github.com>
2024-03-12 15:16:12 +0100

    implement hasDigit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,19 +3,25 @@ package org.example;
 public class Main {
     public static int MIN_LENGTH = 8;
     public static void main(String[] args) {
-        checkLength("abcdefgh");
+
+        System.out.println(checkLength("abcdefgh"));
+        System.out.println(hasDigit("acdfgh"));
     }
 
     public static boolean checkLength(String password) {
         if (password.length() >= MIN_LENGTH) {
-            System.out.println(password.length());
             return true;
         }
         return false;
     }
 
     public static boolean hasDigit(String password) {
-
+        String[] digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        for (int i = 0; i < digits.length; i++) {
+            if (password.contains(digits[i])) {
+                return true;
+            }
+        }
         return false;
     }
 
